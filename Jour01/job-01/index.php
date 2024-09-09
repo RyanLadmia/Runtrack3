d571d6440b46bc6db8b8ca2f18a6c569035c34a1
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-09-09 10:48:10 +0200

    test script dans et hors header

--- Jour01/job-01/index.php
+++ Jour01/job-01/index.php
@@ -5,14 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+</head>
+
+<body>
+    <h1>Bienvenue sur la page</h1>
+    <p>Cette page affiche une popup avec JavaScript.</p>
     <script type="text/javascript">
         window.onload = function() {
             alert('Hello Javascript!');
         };
     </script>
-</head>
-<body>
-    <h1>Bienvenue sur la page</h1>
-    <p>Cette page affiche une popup avec JavaScript.</p>
 </body>
 </html>

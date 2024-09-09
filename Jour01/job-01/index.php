a5d51e6a9231367ebb49d6f8915d48bb0fb4d40e
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-09-09 10:46:23 +0200

    b

--- Jour01/job-01/index.php
+++ Jour01/job-01/index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Job 01</title>
+    <title>Document</title>
+    <script type="text/javascript">
+        window.onload = function() {
+            alert('Hello Javascript!');
+        };
+    </script>
 </head>
 <body>
-    
+    <h1>Bienvenue sur la page</h1>
+    <p>Cette page affiche une popup avec JavaScript.</p>
 </body>
 </html>

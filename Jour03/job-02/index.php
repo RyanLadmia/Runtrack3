a349f434cab0fbc09a328ee8014f0d9d90561c5e
RyanLadmia <ryan.ladmia@laplateforme.io>
2024-09-12 10:42:17 +0200

    job 2 début

---
+++ Jour03/job-02/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" herf="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Lien vers la bibliothèque jQuery -->
+    <script src="script.js" defer></script>
+    <title>Job 2</title>
+</head>
+<body>
+    <button id="shuffle">Shuffle</button>
+    <section id="container" class="conteneur">
+        <img src="arc1.png" id="img1" class="rainbow" alt="img1_arc">
+        <img src="arc2.png" id="img2" class="rainbow" alt="img2_arc">
+        <img src="arc3.png" id="img3" class="rainbow" alt="img3_arc">
+        <img src="arc4.png" id="img4" class="rainbow" alt="img4_arc">
+        <img src="arc5.png" id="img5" class="rainbow" alt="img5_arc">
+        <img src="arc6.png" id="img6" class="rainbow" alt="img6_arc">
+    </section>
+
+    <p id="message"></p>
+
+    <script></script>
+</body>
+</html>

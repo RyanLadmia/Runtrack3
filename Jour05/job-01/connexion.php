96116b987bef06b30cd397b7f2a6b393d753b04c
RyanLadmia <ryan.ladmia@laplateforme.io>
2024-09-12 16:32:14 +0200

    jour 5 job 1 update (not done)

---
+++ Jour05/job-01/connexion.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 1</title>
+</head>
+<body>
+    <a href="inscription.php">Inscription</a><br><br>
+    
+    <h2>Connexion</h2><br><br>
+
+    <form>
+        <label for="email">E-mail</label><br>
+        <input type="email" id="email" name="email"><br><br>
+
+        <label for="password">Mot de passe</label><br>
+        <input type="password" id="password" name="password"><br><br>
+
+        <input type="submit" id="connect" value="Se connecter">
+    </form>
+    
+</body>
+</html>
--- Jour05/job-01/inscription.php
+++ Jour05/job-01/inscription.php
@@ -6,20 +6,31 @@
     <title>Job 1</title>
 </head>
 <body>
+<a href="connexion.php">Connexion</a><br><br>
+
     <h2>Inscription</h2><br><br>
+
     <form>
-        <label for="">Nom</label><br>
-        <input><br><br>
-        <label for="">Prenom</label><br>
-        <input><br><br>
-        <label>E-mail</label><br>
-        <input><br><br>
-        <label>Mot de passe</label><br>
-        <input><br><br>
-        <label>Adresse</label><br>
-        <input><br><br>
-        <label>Code Postal</label><br>
-        <input><br><br>
+        <label for="lastname">Nom</label><br>
+        <input type="text" id="lastname" name="lastname"><br><br>
+
+        <label for="prenom">Prenom</label><br>
+        <input type="text" id="firstname" name="firstname"><br><br>
+
+        <label for="email">E-mail</label><br>
+        <input type="email" id="email" name="email"><br><br>
+
+        <label for="password">Mot de passe</label><br>
+        <input type="password" id="password" name="password"><br><br>
+
+        <label for="adress">Adresse</label><br>
+        <input type="text" id="adress" name="adress"><br><br>
+
+        <label for="zipcode">Code Postal</label><br>
+        <input type="number" id="zipcode" name="zipcode"><br><br>
+
+        <input type="submit" id="submit" value="Valider l'inscription">
     </form>
+
 </body>
 </html>

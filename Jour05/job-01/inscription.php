f0d4370333b8bb99c3d5e0a5e67291cba54e6a54
RyanLadmia <ryan.ladmia@laplateforme.io>
2024-09-12 16:16:56 +0200

    début jour 5

---
+++ Jour05/job-01/inscription.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 1</title>
+</head>
+<body>
+    <h2>Inscription</h2><br><br>
+    <form>
+        <label for="">Nom</label><br>
+        <input><br><br>
+        <label for="">Prenom</label><br>
+        <input><br><br>
+        <label>E-mail</label><br>
+        <input><br><br>
+        <label>Mot de passe</label><br>
+        <input><br><br>
+        <label>Adresse</label><br>
+        <input><br><br>
+        <label>Code Postal</label><br>
+        <input><br><br>
+    </form>
+</body>
+</html>

1ca153f7324d3bdc5558a065cd97adacab09dd22
RyanLadmia <ryan.ladmia@laplateforme.io>
2024-09-19 16:32:38 +0200

    desktop ok

--- Jour06/job-03/index.php
+++ Jour06/job-03/index.php
@@ -1,20 +1,43 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Job 3</title>
-     <!-- Lien vers Bootstrap -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-     <!-- Lien vers Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Lien vers jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <!-- Lien vers Bootstrap Icons -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <title>Maquette Responsive</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-    
+    <div class="container">
+        <!-- Ligne 1 -->
+        <div class="row">
+            <div class="col-12 block"></div>
+        </div>
+        <!-- Ligne 2 -->
+        <div class="row">
+            <div class="col-lg-4 col-12 block"></div>
+            <div class="col-lg-4 col-12 block"></div>
+            <div class="col-lg-4 col-12 block"></div>
+        </div>
+        <!-- Ligne 3 -->
+        <div class="row">
+            <div class="col-lg-8 col-12 block"></div>
+            <div class="col-lg-4 col-12 block"></div>
+        </div>
+        <!-- Ligne 4 -->
+        <div class="row">
+            <div class="col-lg-2 col-2 block"></div>
+        </div>
+        <!-- Ligne 5 -->
+        <div class="row justify-content-center">
+            <div class="col-lg-2 col-2 block"></div>
+        </div>
+        <!-- Ligne 6 -->
+        <div class="row justify-content-end">
+            <div class="col-lg-2 col-2 block"></div>
+        </div>
+    </div>
+
 </body>
 </html>

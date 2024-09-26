288265460c3ea35f8878d823efa3b88f8847aa23
RyanLadmia <ryan.ladmia@laplateforme.io>
2024-09-26 16:24:24 +0200

    Jour 7 done

---
+++ Jour07/job-05/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Materialize CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <title>Job 05</title>
+</head>
+
+<body>
+  
+  <!-- HEADER -->
+  <header>
+    <nav class="blue lighten-1">
+      <div class="nav-wrapper container">
+        <a href="#" class="brand-logo logo"><i class="material-icons">flag</i>Mugiwara</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="index.php">Accueil</a></li>
+          <li><a href="index.php">Services</a></li>
+          <li><a href="index.php">À propos</a></li>
+          <li><a href="index.php">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  
+  <main class="container">
+    <div class="row">
+      <!-- Carte 1 -->
+      <div class="col s12 m6 l4">
+        <div class="card small">
+          <div class="card-image">
+            <img src="sanji.jpg" alt="Image de Sanji">
+          </div>
+          <div class="card-content">
+            <p>Vinsmoke Sanji</p>
+          </div>
+          <div class="card-action">
+            <a href="#">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Carte 2 -->
+      <div class="col s12 m6 l6">
+        <div class="card medium">
+          <div class="card-image">
+            <img src="zoro.jpg" alt="Image de Zoro">
+          </div>
+          <div class="card-content">
+            <p>Roronoa Zoro</p>
+          </div>
+          <div class="card-action">
+            <a href="#">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Carte 3 -->
+      <div class="col s12 l8">
+        <div class="card large">
+          <div class="card-image">
+            <img src="luffy.jpg" alt="Image de Luffy">
+          </div>
+          <div class="card-content">
+            <p>Monkey D Luffy</p>
+          </div>
+          <div class="card-action">
+            <a href="#">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <!-- FOOTER -->
+  <footer class="page-footer blue lighten-1">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Footer Content</h5>
+          <p class="grey-text text-lighten-4">Vous pouvez utiliser ce pied de page pour inclure des informations importantes.</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Liens</h5>
+          <ul>
+            <li><a class="grey-text text-lighten-3" href="index.php">Accueil</a></li>
+            <li><a class="grey-text text-lighten-3" href="index.php">Services</a></li>
+            <li><a class="grey-text text-lighten-3" href="index.php">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © 2024 Mon Site Web
+        <a class="grey-text text-lighten-4 right" href="index.php">Plus de liens</a>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Materialize JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+
+</html>

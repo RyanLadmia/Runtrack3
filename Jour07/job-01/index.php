d66634edb5b353df704d0daff873cb3b2c4e157b
RyanLadmia <ryan.ladmia@laplateforme.io>
2024-09-26 14:25:08 +0200

    header

--- Jour07/job-01/index.php
+++ Jour07/job-01/index.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
+    <!-- Font Awesome for icons (optional) -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <title>Job 01</title>
 </head>
+
 <body>
-    
+    <header>
+        <nav>
+            <div class="nav-wrapper">
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="index.php">Connexion</a></li>
+                    <li><a href="index.php">Inscription</a></li>
+                    <li><a href="index.php">Recherche</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
+
 </html>
